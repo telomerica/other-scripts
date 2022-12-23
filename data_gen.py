fb14ead603947d77659053f306fcc7c05842e925
CHraven <64539817+telomerica@users.noreply.github.com>
2022-12-23 22:31:15 +0100

    Add files via upload

--- data_gen.py
+++ data_gen.py
@@ -2,34 +2,41 @@ import numpy as np
 import random
 from perlin_noise import PerlinNoise
 
-def sigmoid(ls):
-    return 1 / (1+np.exp(-ls))
+def sigmoid(array):
+    return 1 / (1+np.exp(-array))
 
-def bias_(ls,bias):
-    return ls+bias
+def bias_(array,bias):
+    return array+bias
+
+def random_(grid):
+    ls = []
+    biasdirection = random.choice([1,-1])
+    bias = random.random()*biasdirection
+    for row in range(grid[0]):
+        ls1 = []
+        for column in range(grid[1]):
+            ls1.append(random.random()+bias)
+        ls.append(ls1)
+    return ls
+
+def perlin_(grid,octaves,seed=False):
+    perlin = PerlinNoise(octaves=octaves, seed=random.randint(0,1000))
+    array = ([[perlin([_/grid[0], __/grid[1]]) for __ in range(grid[0])] for _ in range(grid[1])])
+    return array
 
 def data_generator(grid,pattern,options):
     
     if pattern[0]=="random":
-        ls = []
-        biasdirection = random.choice([1,-1])
-        bias = random.random()*biasdirection
-        for row in range(grid[0]):
-            ls1 = []
-            for column in range(grid[1]):
-                ls1.append(random.random()+bias)
-            ls.append(ls1)
+        mappe = random_(grid)
         
     elif pattern[0]=="patches":
-        perlin = PerlinNoise(octaves=pattern[1], seed=random.randint(0,1000))
-        ls = ([[perlin([_/grid[0], __/grid[1]]) for __ in range(grid[0])] for _ in range(grid[1])])
-    
-    ls = np.array(ls)
+        mappe = perlin_(grid=grid,octaves=pattern[1])
+    mappe = np.array(mappe)
     
     if options[0]=="sigmoid":
-        ls = sigmoid(ls)
+        mappe = sigmoid(mappe)
     
     elif options[0]=="bias":
-        ls = bias_(ls,options[1])
+        mappe = bias_(mappe,options[1])
         
-    return ls
+    return mappe
