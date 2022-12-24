be330a73bdda796b31260277964df746519b54d4
CHraven <64539817+telomerica@users.noreply.github.com>
2022-12-24 03:23:53 +0100

    Add files via upload

---
+++ img_map.py
@@ -0,0 +1,23 @@
+from PIL import Image, ImageDraw
+import random
+import numpy as np
+
+def draw_map(map_array,color_dict=None):
+    if color_dict==None:
+        color_dict = {0:(120,180,60),1:(105,119,46),2:(157,206,243),3:(0,0,0)}
+    img = Image.new("RGB", (map_array.shape))
+    img1 = ImageDraw.Draw(img)  
+    img1.rectangle( [(0,0),(map_array.shape)] ,fill="white",outline="white")
+    
+    for element in color_dict:
+        b,a = np.where(map_array==element)
+        color = color_dict.get(element)
+        for index,x in enumerate(a):
+            y = b[index]
+            img.putpixel((x,y),(color))
+            
+    nym = ""
+    for x in range(11):
+        nym = nym+str(random.randint(0,10))
+    img.save(f"maps/{nym}.png")
+    img.show()
