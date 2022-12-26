a87330c37bf541914ea2a6072ea3af0857363eda
CHraven <64539817+telomerica@users.noreply.github.com>
2022-12-26 15:48:04 +0100

    Add files via upload

--- img_map.py
+++ img_map.py
@@ -3,7 +3,7 @@ import random
 import numpy as np
 import os
 
-def draw_map(map_array,save=False,color_dict=None):
+def draw_map(map_array,color_dict=None):
     if color_dict==None:
         color_dict = {0:(120,180,60),1:(105,119,46),2:(157,206,243),3:(0,0,0)}
     img = Image.new("RGB", (map_array.shape))
@@ -16,13 +16,28 @@ def draw_map(map_array,save=False,color_dict=None):
         for index,x in enumerate(a):
             y = b[index]
             img.putpixel((x,y),(color))
+    return img
 
-     
-    nym = ""
-    for x in range(5):
-        nym = nym+str(random.randint(0,10))
-    img.save(f"maps/{nym}.png")
-    img.show()
-    if save==True:
-        with open(f'{os.getcwd()}/saved_arrays/{nym}.npy', 'wb') as f:
-            np.save(f, map_array)
+
+def save_map(map_array):
+    zeroeth = np.flip(map_array,0)
+    oneth = np.flip(map_array,1)
+    z_one = np.flip(zeroeth,1)
+    o_zero = np.flip(oneth,0)
+
+    all_arrays = [map_array,zeroeth,oneth,z_one,o_zero]
+    extend_list = ["f0","f1","f2","f3","f4"]
+        
+    ltr_list = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    nmr_list = "1234567890"
+    ltr1,ltr2,ltr3 = random.sample(ltr_list,3)
+    nmr1,nmr2,nmr3 = random.sample(nmr_list,3)
+
+    for i in range(len(all_arrays)):
+        extend = extend_list[i]
+        generated_name = f"{extend}_{ltr1}{ltr2}{ltr3}{nmr1}{nmr2}{nmr3}"
+        img = draw_map(all_arrays[i])        
+        img.save(f"saved_images/{generated_name}.png")
+        
+        with open(f'{os.getcwd()}/saved_arrays/{generated_name}.npy', 'wb') as f:
+            np.save(f, all_arrays[i])
