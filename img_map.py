acca02bc410d5080cd26f3104097f0057e6e7fa0
CHraven <64539817+telomerica@users.noreply.github.com>
2022-12-26 01:12:17 +0100

    Add files via upload

--- img_map.py
+++ img_map.py
@@ -1,8 +1,9 @@
 from PIL import Image, ImageDraw
 import random
 import numpy as np
+import os
 
-def draw_map(map_array,color_dict=None):
+def draw_map(map_array,save=False,color_dict=None):
     if color_dict==None:
         color_dict = {0:(120,180,60),1:(105,119,46),2:(157,206,243),3:(0,0,0)}
     img = Image.new("RGB", (map_array.shape))
@@ -15,8 +16,13 @@ def draw_map(map_array,color_dict=None):
         for index,x in enumerate(a):
             y = b[index]
             img.putpixel((x,y),(color))
-            
+
+     
     nym = ""
     for x in range(5):
         nym = nym+str(random.randint(0,10))
     img.save(f"maps/{nym}.png")
+    img.show()
+    if save==True:
+        with open(f'{os.getcwd()}/saved_arrays/{nym}.npy', 'wb') as f:
+            np.save(f, map_array)
