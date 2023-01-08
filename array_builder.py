b975d45eea6db21fe73effd8e25a91df48966b9d
CHraven <64539817+telomerica@users.noreply.github.com>
2023-01-08 18:18:38 +0100

    Add files via upload

--- array_builder.py
+++ array_builder.py
@@ -1,47 +1,40 @@
 import numpy as np
 import random
 from perlin_noise import PerlinNoise
-from img_map import *
 
+def array_builder(map_array, steps):
+    temp_array = map_array
+    for step in steps:
+        temp_array=step["method"](temp_array,**step["args"])
+    return temp_array
 
-def sigmoid_(array):
+def sigmoid_(array,float=False):
     return 1 / (1+np.exp(-array))
 
-def bias_(array,bias):
+def bias_(array,bias,float=False):
     return array+bias
 
-def random_(array,probability,choice):
-    iter = int((array.shape[0]*array.shape[1])*probability)
+def random_(array,probability,float=False):
+    x = array
+    for row in x:
+        coords = np.random.randint(0,len(row),size=(int(len(row)*probability)))
+        r = np.random.rand(*row.shape)
+        row[coords] = r[coords]
+        #print(row)
+    return x
 
-    for i in range(iter):
-        array[random.randrange(0,array.shape[0]-1)][random.randrange(0,array.shape[1]-1)]=choice
-    return array
-
-
-def perlin_(array,octaves,seed,object,bias=0):
+def perlin_(array,octaves,seed,object,bias=0,float=False):
     grid = array.shape
     perlin = PerlinNoise(octaves=octaves, seed=seed)
     temp = ([[perlin([_/grid[0], __/grid[1]]) for __ in range(grid[0])] for _ in range(grid[1])])
     temp = np.array(temp)+bias
-    temp = (np.rint(sigmoid_(temp))).astype(int)
     
-    temp_w = np.where(temp==1)
-    for x in range(len(temp_w[0])):
-        array[temp_w[0][x]][temp_w[1][x]] = object
-
+    if float==False:
+        temp = (np.rint(sigmoid_(temp))).astype(int)
+        temp_w = np.where(temp==1)
+        for x in range(len(temp_w[0])):
+            array[temp_w[0][x]][temp_w[1][x]] = object
+    else:
+        array = array+temp
     return array
 
-def array_build(grid,functionslist):
-    mappe = np.zeros(grid)
-    #print(mappe)
-
-    dict_of_functions={"random":random_,"perlin":perlin_,"sigmoid":sigmoid_,"bias":bias_}
-
-    print(functionslist)
-    for fc in functionslist:
-        #print(fc[1])
-        #print(mappe,*fc[1])
-
-        function = dict_of_functions.get(fc[0])
-        mappe = function(mappe,*fc[1]) 
-    return mappe
--- img_map.py
+++ img_map.py
@@ -3,41 +3,55 @@ import random
 import numpy as np
 import os
 
-def draw_map(map_array,color_dict=None):
+def draw_map(map_array,color_dict=None,float=False):
     if color_dict==None:
-        color_dict = {0:(120,180,60),1:(105,119,46),2:(157,206,243),3:(0,0,0)}
+        color_dict = {0:(11,180,60),1:(105,119,46),2:(157,206,243),3:(0,0,0)}
+    
+    if float==True:
+        color_dict = {}
+        for i in range(255):
+            color_dict[i]=(i,i,i)
+        map_array = np.rint(map_array*255)
+
     img = Image.new("RGB", (map_array.shape))
     img1 = ImageDraw.Draw(img)  
     img1.rectangle( [(0,0),(map_array.shape)] ,fill="white",outline="white")
-    
+
     for element in color_dict:
         b,a = np.where(map_array==element)
         color = color_dict.get(element)
         for index,x in enumerate(a):
             y = b[index]
             img.putpixel((x,y),(color))
+    
     return img
 
 
-def save_map(map_array):
+def save_map(map_array,color_dict=None,draw=True,float=False):
     zeroeth = np.flip(map_array,0)
     oneth = np.flip(map_array,1)
     z_one = np.flip(zeroeth,1)
-    o_zero = np.flip(oneth,0)
 
-    all_arrays = [map_array,zeroeth,oneth,z_one,o_zero]
-    extend_list = ["f0","f1","f2","f3","f4"]
+    all_arrays = [map_array,zeroeth,oneth,z_one]
+    extend_list = ["f0","f1","f2","f3"]
             
     already_saved = (os.listdir("saved_arrays"))
-    already_saved.sort()
-    last = already_saved[-1]
-    last_number, extension = last.split("-")
+    a_ls = []
+    for i in already_saved:
+        l,b = i.split("-")
+        a_ls.append(int(l))
+
+
+    a_ls.sort()
+    last_number = int(a_ls[-1])
+    next_number = last_number+1
 
     for i in range(len(all_arrays)):
         extend = extend_list[i]
-        generated_name = f"{int(last_number)+1}-{extend}"
-        img = draw_map(all_arrays[i])        
-        img.save(f"saved_images/{generated_name}.png")
+        generated_name = f"{next_number}-{extend}"
+        if draw==True:
+            img = draw_map(all_arrays[i],color_dict,float)        
+            img.save(f"saved_images/{generated_name}.png")
         
         with open(f'{os.getcwd()}/saved_arrays/{generated_name}.npy', 'wb') as f:
             np.save(f, all_arrays[i])
