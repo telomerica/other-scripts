0fbd97f9c2bc1e55b5005c9727b4138213900aaf
YagizBal <64539817+yagizbal@users.noreply.github.com>
2023-05-05 04:11:17 +0200

    Update convoluter.py

--- convoluter.py
+++ convoluter.py
@@ -2,30 +2,27 @@ def convert_img(directory):
     img = mpimg.imread(directory)
     img = np.array(img)
     array = np.dot(img[..., :3], [0.2989, 0.5870, 0.1140])
-
     return array
 
-def convolute(array,kernel,stride,return_image=False):
-    rows = array.shape[0]
-    columns = array.shape[1]
 
-    ls = []
-    for row in range(rows-kernel):
-        ls1=[]
-        for i in range(columns-kernel):
-            tile_value=np.sum(array[0+(row*stride):kernel+(row*stride),0+(i*stride):kernel+(stride*i)])/(kernel*kernel)
-            ls1.append(tile_value)
-        ls.append(ls1)
-    ls=np.array(ls)
-    #if return_pillow_image:
+
+def convolute(array, kernel, stride):
+    rows, columns = array.shape
+    out_rows = (rows - kernel) // stride + 1
+    out_columns = (columns - kernel) // stride + 1
+
+    strides = (array.strides[0] * stride, array.strides[1] * stride, *array.strides)
+    strided_array = as_strided(array, shape=(out_rows, out_columns, kernel, kernel), strides=strides)
+
+    ls = np.average(strided_array, axis=(2, 3))
+
     return ls
-
 
 #kernel_size = 20
 #stride = 1
 
-#array = convert_img("image.png")
-#result = convolute(kernel=kernel_size, stride=stride, array=array, return_image=True)
+#array = convert_img("imgae.png")
+#result = convolute(kernel=kernel_size, stride=stride, array=array)
 
 #plt.imshow(result, cmap='gray')
 #plt.show()
